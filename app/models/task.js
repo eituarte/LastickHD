d5fe13d36042e1ba01723b317dca70b874acd57c
henryituarte <henryituarte@gmail.com>
2017-09-30 00:53:20 +0200

    Solving jigsaw puzzle

--- app/models/task.js
+++ app/models/task.js
@@ -20,16 +20,15 @@ var taskSchema = mongoose.Schema({
 
 taskSchema.methods.addTask = function(request, response){
 
-	this.task.name = request.body.name;
-	console.log("Ha funcionado");
-	/*this.task.email = request.body.email;
-	this.task.dateIni = request.body.dateIni;
-	this.task.dateEnd = request.body.dateEnd;
-	this.task.acr = request.body.acr;
-	this.task.desc = request.body.desc;
-	this.task.tags = request.body.tags;
-	this.task.comments = request.body.comments;
-	this.task.repeat = request.body.repeat;*/
+	this.task.name = request.body.name_task;
+	this.task.dateIni = request.body.time_ini_task;
+	this.task.dateEnd = request.body.time_fin_task;
+	this.task.acr = request.body.acr_task;
+	this.task.desc = request.body.desc_task;
+	this.task.tags = request.body.tags_task;
+	this.task.comments = request.body.comments_task;
+	//this.task.repeat = request.body.repeat;
+	console.log("rula");
 	this.task.save();
 };
 
--- app/models/user.js
+++ app/models/user.js
@@ -3,9 +3,9 @@ var bcrypt   = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema({
     user             : {
-	username     :String,
-        email        : String,
-        password     : String,
+	username     : String,
+    email        : String,
+    password     : String,
 	name	     : String,
 	address      : String
     }
